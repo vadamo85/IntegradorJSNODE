d4e5f61fdee27c8d3367146097ff85ef7a56cdcb
vadamo85 <valeriaxadamo@gmail.com>
2023-12-05 00:01:40 -0300

    Misión 4 cont.

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public_html'));
-
-app.get('/ping', (req, res) => res.send('pong'));
+app.use(express.static('public'));
 
 app.listen(4000, () => console.log("Servidor corriendo en http:localhost:4000"));
